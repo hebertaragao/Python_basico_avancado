ec876d65837a5a5f87ff70ce65fa51b1e8c33b51
hebertaragao <hebert.aragao@outlook.com>
2023-01-02 21:39:19 -0300

    falta correção na linha 10
    Signed-off-by: hebertaragao <hebert.aragao@outlook.com>

--- poo/desafio_loja.py
+++ poo/desafio_loja.py
@@ -7,7 +7,7 @@ def main():
     cliente = Cliente('Maria Lima', 44)
     vendedor = Vendedor('Pedro Garrido', 36, 5000)
     compra1 = Compra(cliente, datetime.now(), 512)
-    compra2 = Compra(cliente, datetime(2018, 6, 4, 256))
+    compra2 = Compra(cliente, datetime(2018, 6, 4) 256)
     cliente.registrar_compra(compra1)
     cliente.registrar_compra(compra2)
     print(f'Cliente: {cliente}', '(adulto)' if cliente.is_adulto()else '')
