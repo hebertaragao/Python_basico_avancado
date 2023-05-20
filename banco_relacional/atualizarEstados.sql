ac15e0e1f5b0d7c09dc123abbdc0dc0714159a53
hebertaragao <hebert.aragao@outlook.com>
2023-05-20 00:21:20 -0300

    inserido atualizarEstados.sql

---
+++ banco_relacional/atualizarEstados.sql
@@ -0,0 +1,9 @@
+update estados
+set nome = "Maranhão"
+WHERE sigla = "MA"
+
+select nome from estados WHERE sigla = 'MA'
+
+update estados 
+set nome = 'Paraná', populacao = 11.32
+WHERE sigla = 'PR'
