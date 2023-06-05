638629db8dad03b395c5c0bf7de86a70b1da7266
hebertaragao <hebert.aragao@outlook.com>
2023-06-04 22:24:02 -0300

    criado ordenar_contatos.py

---
+++ banco_dados/ordenar_contatos.py
@@ -0,0 +1,10 @@
+from bd import nova_conexao
+
+sql = 'SELECT nome FROM contatos ORDER BY nome DESC'
+
+with nova_conexao() as conexao:
+    cursor = conexao.cursor()
+    cursor.execute(sql)
+
+    print('\n'.join(registro[0] for registro in cursor))
+    
